2eae09aaf41c1f1cb7c541d7c801d3121d695b4b
tw_yshuang <rober5566a@gmail.com>
2020-12-05 23:18:49 +0800

    [adj] Automatic-Installation_package.sh, Automatic-Installation_step2.sh

--- Automatic-Installation_package.sh
+++ Automatic-Installation_package.sh
@@ -1,14 +1,20 @@
-sudo apt install update
-sudo apt install upgrade
-sudo apt install git-all
-sudo apt install vim
+sudo apt  update
+sudo apt upgrade
+sudo apt install git-all -y
+sudo apt install vim -y
+sudo apt install curl -y
+
+# install pyenv
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
 # install vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get install code -y
 
 # install chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
