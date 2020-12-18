096b47afd00774d7ba58606910a4fb3b31bb2da3
tw_yshuang <rober5566a@gmail.com>
2020-12-19 04:19:27 +0800

    [rename][mod] rename: 	Automatic-Installation_application.sh->application.sh, 	Automatic-Installation_package.sh->package.sh mod: 	package.sh, add command: "git lg"~

---
+++ application.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+#>            +------------------+
+#>            |  application.sh  |
+#>            +------------------+
+#-
+#- SYNOPSIS
+#-
+#-    ./application.sh [-h]
+#-
+#- OPTIONS
+#-
+#-    -y,                    all accept.
+#-    -h, --help             print help information.
+#-
+#- EXAMPLES
+#-
+#-    $ ./application.sh -y
+
+#====================================================
+# Part 1. Option Tool (DO NOT MODIFY)
+#====================================================
+# Print script help
+function show_script_help(){
+    echo
+    head -17 $0 | # find this file top 16 lines.
+    grep "^#[-|>]" | # show the line that include "#-" or "#>".
+    sed -e "s/^#[-|>]*//1" # use nothing to replace "#-" or "#>" that the first keyword in every line.  
+    echo 
+}
+# Receive arguments in slient mode.
+all_accept=0
+if [ "$#" -gt 0 ]; then
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            # Help
+            "-h"|"--help")
+                show_script_help
+                exit 1
+            ;;
+            # All Accept
+            "-y")
+                all_accept=1
+                shift 1
+            ;;
+        esac
+    done
+fi
+
+function Ask_yn(){
+    printf "$1 [y/n]" 
+    if [ $all_accept = 1 ]; then
+        printf "-y"
+        return 1
+    fi
+    read respond
+    if [ "$respond" = "y" -o "$respond" = "Y" -o "$respond" = "" ]; then
+        return 1
+    elif [ "$respond" = "n" -o "$respond" = "N" ]; then
+        return 0
+    else
+        echo 'wrong command!!'
+        Ask_yn $1
+        return $?
+    fi
+    unset respond
+}
+
+#====================================================
+# Part 2. Main
+#====================================================
+sudo apt update
+sudo apt upgrade
+
+declare -A Application_dict
+Application_dict=([code]="VScode" [google-chrome]="Chrome" [vlc]="VLC")
+
+for key in ${!Application_dict[*]}; do
+    app_root="$(command -v $key)" # get application root
+    if [ "$app_root" = "" ]; then
+        Ask_install "Do you wamt to install ${Application_dict[$key]}"; result=$? # get Ask_install() return {Application_dict[$key]}
+        if [ $result = 1 ]; then
+            case ${Application_dict[$key]} in 
+                $Application_dict[code]) # install VScode
+                    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+                    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+                    sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+                    sudo apt-get install apt-transport-https -y
+                    sudo apt-get update
+                    sudo apt-get install code -y
+                    ;;
+                $Application_dict[google-chrome]) # install Chrome
+                    wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                    sudo dpkg -i google-chrome-stable_current_amd64.deb
+                    sudo apt-get install -f
+                    ;;
+                $Application_dict[vlc]) # install VLC
+                    sudo apt-get install vlc -y
+                    ;;
+            esac
+        fi
+    else
+        echo "You are already installed ${Application_dict[$key]} before~"
+    fi
+done
---
+++ package.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+#>            +------------------+
+#>            |    package.sh    |
+#>            +------------------+
+#-
+#- SYNOPSIS
+#-
+#-    ./package.sh [-h]
+#-
+#- OPTIONS
+#-
+#-    -y,                    all accept.
+#-    -h, --help             print help information.
+#-
+#- EXAMPLES
+#-
+#-    $ ./package.sh -y
+
+#====================================================
+# Part 1. Option Tool (DO NOT MODIFY)
+#====================================================
+# Print script help
+function show_script_help(){
+    echo
+    head -17 $0 | # find this file top 16 lines.
+    grep "^#[-|>]" | # show the line that include "#-" or "#>".
+    sed -e "s/^#[-|>]*//1" # use nothing to replace "#-" or "#>" that the first keyword in every line.  
+    echo 
+}
+# Receive arguments in slient mode.
+all_accept=0
+if [ "$#" -gt 0 ]; then
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            # Help
+            "-h"|"--help")
+                show_script_help
+                exit 1
+            ;;
+            # All Accept
+            "-y")
+                all_accept=1
+                shift 1
+            ;;
+        esac
+    done
+fi
+
+function Ask_yn(){
+    printf "$1 [y/n]" 
+    if [ $all_accept = 1 ]; then
+        printf "-y"
+        return 1
+    fi
+    read respond
+    if [ "$respond" = "y" -o "$respond" = "Y" -o "$respond" = "" ]; then
+        return 1
+    elif [ "$respond" = "n" -o "$respond" = "N" ]; then
+        return 0
+    else
+        echo 'wrong command!!'
+        Ask_yn $1
+        return $?
+    fi
+    unset respond
+}
+
+#====================================================
+# Part 2. Main
+#====================================================
+sudo apt update
+sudo apt upgrade
+sudo apt install -y git-all vim curl
+
+# git: config global usr information
+Ask_yn "Do you want to config global user information?"; result=$?
+if [ $result = 1 ]; then
+    printf "Enter your global user name: "; read usr_name
+    printf "Enter your global user mail: "; read usr_mail
+    git config --global user.name "$usr_name"
+    git config --global user.email "$usr_mail"
+fi
+
+# git: config vim to the commit editor
+Ask_yn "Do you want use VIM to be your editor?"; result=$?
+if [ $result = 1 ]; then
+    git config --global core.editor vim
+fi
+
+#git: add command "git lg"
+Ask_yn "Do you want add command: 'git lg'?"; result=$?
+if [ $result = 1 ]; then
+    FILE_PATH="$HOME/ubuntu_costomer_setting/Automatic-Installation_terminal-setting/14.txt"
+    Keyword="[alias]"
+    CUS_Command='lg = !"git lg2"'
+
+    SRC_arr=("[alias]" "lg1 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all" "lg2 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all" "$CUS_Command")
+
+    function Merge_SRC(){
+        local SRC=""
+        for key in ${!SRC_arr[*]}; do
+            if [ $key -lt $1 ];then
+                :
+            else 
+                SRC="$SRC${SRC_arr[key]}\n"
+            fi
+        done
+        echo "$SRC"
+    }
+
+    if [ -f $FILE_PATH ]; then
+        if grep -Fn "$Keyword" $FILE_PATH; then
+            sed -i "/$Keyword/a $(Merge_SRC 1)" $FILE_PATH
+        else
+            printf "%b" "\n$(Merge_SRC 0)" >> $FILE_PATH
+        fi
+    else
+        printf "%b" "$(Merge_SRC 0)" >> $FILE_PATH
+    fi
+fi
+
+sudo apt-get autoremove
+echo "Done!!"
