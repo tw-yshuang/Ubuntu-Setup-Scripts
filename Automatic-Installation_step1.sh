0eb57032b09ede86b0c0d902e94844642f117d08
tw_yshuang <rober5566a@gmail.com>
2020-12-19 14:43:09 +0800

    [mod] mod: Automatic-Installation_step1.sh, use sed to edit ~/.zshrc file.

--- Automatic-Installation_step1.sh
+++ Automatic-Installation_step1.sh
@@ -4,14 +4,16 @@ chsh -s $(which zsh)
 
 # oh-my-zsh package install
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
-# sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-p10k configure
+sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 # sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" | exit
 echo "=======================================done==================================="
+
 # TODO: use sed to edit ~/.zshrc, ~/.p10k.zsh
+sed -ie 's/ZSH_THEME=*/ZSH_THEME="powerlevel10k/powerlevel10k"/' -ie 's/^plugins/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+printf "# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.\n[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
+
 
 # terminal theme setting
 cp ttf/*ttf ~/.local/share/fonts/
@@ -19,7 +21,7 @@ fc-cache -f -v
 dconf load /org/gnome/terminal/ < gnome_terminal_settings_backup.txt
 
 
-# remove comand
+# remove command
 sudo apt remove zsh -y
 sudo apt autoremove
 sudo rm -rf /home/ys_huang/.oh-my-zsh
