6ba2478af5113210451a43dd9c4c952b7924d222
tw_yshuang <rober5566a@gmail.com>
2020-12-19 09:04:48 +0800

    [add][mod] add: 	file: ttf, to save .ttf file mod: 	Automatic-Installation_step1.sh, this file will setting all packages & configs reated about terminal & zsh.

--- Automatic-Installation_step1.sh
+++ Automatic-Installation_step1.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
-echo "installing zsh"
+#!/usr/bin/env bash
 sudo apt install zsh
 chsh -s $(which zsh)
-echo "Done!! please restart your terminal~"
+
+# oh-my-zsh package install
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# TODO: use sed to edit ~/.zshrc, ~/.p10k.zsh
+
+# terminal theme setting
+cp ttf/*ttf ~/.local/share/fonts/
+fc-cache -f -v 
+dconf load /org/gnome/terminal/ < gnome_terminal_settings_backup.txt
