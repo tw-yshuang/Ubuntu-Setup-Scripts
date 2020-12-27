4c6e29f736ef286167cc971d5cd90ae72ef76b5a
tw_yshuang <rober5566a@gmail.com>
2020-12-28 02:22:55 +0800

    [mod] mod: zsh_ohmyzsh_setup.sh, adjustment shell auto run zsh let shell script can't contorl when ohmyzsh is install, now can install it and contorlable!!

--- zsh_ohmyzsh_setup.sh
+++ zsh_ohmyzsh_setup.sh
@@ -5,4 +5,4 @@ sudo apt install zsh -y
 chsh -s $(which zsh)
 
 # oh-my-zsh package install
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
