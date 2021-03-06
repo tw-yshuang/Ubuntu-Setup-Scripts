87815c8b0f6e1a0d6936323bbd6727fd1e7b7d5b
tw_yshuang <rober5566a@gmail.com>
2021-03-06 14:55:18 +0800

    [mod] all_setup.sh, chmod +x nvm_setup.sh

--- all_setup.sh
+++ all_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-chmod +x package.sh zsh_ohmyzsh_setup.sh ohmyzsh_config.sh terminal_config.sh application.sh pyenv_setup.sh
+chmod +x package.sh zsh_ohmyzsh_setup.sh ohmyzsh_config.sh terminal_config.sh application.sh pyenv_setup.sh nvm_setup.sh
 ./package.sh -y
 ./zsh_ohmyzsh_setup.sh
 ./ohmyzsh_config.sh -y
