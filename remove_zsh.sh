75ccedcfb0001fad7b651e9e226fec2a7e7b051f
tw_yshuang <rober5566a@gmail.com>
2021-03-06 14:47:24 +0800

    [fix] fix: remove_zsh.sh, fix the path of .oh-my-zsh file.

--- remove_zsh.sh
+++ remove_zsh.sh
@@ -3,7 +3,7 @@
 # remove command
 sudo apt remove zsh -y
 sudo apt autoremove
-sudo rm -rf /home/ys_huang/.oh-my-zsh
+sudo rm -rf /home/$USER/.oh-my-zsh
 sudo rm -rf ~/*.*zsh*
 sudo rm -rf /themes/powerlevel10k
 sudo rm -rf /plugins/zsh-autosuggestions
